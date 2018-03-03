ba3e622c5cc8257bfc047c566e1325eb75d9827a
Maryam81609 <maryam.dabaghchian@gmail.com>
2018-03-02 23:05:51 -0700

    if test

--- 4/generator_tests/mytest.java
+++ 4/generator_tests/mytest.java
@@ -1,11 +1,27 @@
 class MainClass {
     public static void main(String[] a) {
-	// Test PLUS
-	System.out.println(7+2);
-	// Test PLUS
+
+	// Test if and true and false
+	if(true)
+	{
+		System.out.println(11);
+	}	
+	else
+	{
+		System.out.println(10);
+	}
+
+	// Test block
+	//{
+	//	System.out.println(70-2*10);
+	//	System.out.println(70-2-38);
+	//}
 
 	// Test PLUS
+	//System.out.println(7+2);
 
+	// Test MINUS
+	//System.out.println(7-2);
 
 	// Test integer literal
 	//System.out.println(7);
